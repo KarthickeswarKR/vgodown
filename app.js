75ac4c65f9d1b56b9765254eb20a2f8a6573a95e
Karthick Eswar <karthickeswar@virtualgodown.com>
2016-10-21 21:07:39 +0530

    app

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var logger = require('morgan');
 var cron = require('node-schedule');
-var auditLog = require('audit-log');
-var bodyParser = require('body-parser');var auditLog = require('audit-log');
+var bodyParser = require('body-parser');
 var fs = require('fs')
 var config = require('./app/config/config');
 var mongoose = require('mongoose');
 var db = require('./app/db/mongoose');
-auditLog.addTransport("mongoose", {connectionString: config.get('mongoose:uri')});
 var users = require('./app/controllers/user-controller');
 var vg = express();
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
@@ -17,5 +15,5 @@ vg.use(bodyParser.urlencoded({ extended: false }));
 vg.use('/static', express.static(__dirname + '/public'));
 vg.use('/api/users', users);
 module.exports = vg;
-vg.listen(3000);
+vg.listen(80);
 console.log("vg is started and listening 3000");
